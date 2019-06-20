d0783bcd97d98e0a272364614b4bc14ac8260e65
Garry Newman <garrynewman@users.noreply.github.com>
2019-06-20 11:46:37 +0100

    Controller tests

---
+++ Assets/ControllerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+	public GameObject ControllerPrefab;
+
+	public List<ControllerPanel> Controllers = new List<ControllerPanel>();
+
+    void Update()
+    {
+		if ( !Steamworks.SteamClient.IsValid )
+			return;
+
+		Steamworks.SteamInput.RunFrame();
+
+		//
+		// We could probably get away with doing this every couple of seconds
+		// It only needs to be called to keep the list of currently active controllers up to date
+		//
+
+		foreach ( var controller in Steamworks.SteamInput.Controllers )
+		{
+			UpdateController( controller );
+		}
+
+		// TODO - Remove unplugged controllers
+    }
+
+	void UpdateController( Steamworks.Controller controller )
+	{
+		foreach ( var c in Controllers )
+		{
+			if ( c.Controller == controller )
+				return;
+		}
+
+		var o = GameObject.Instantiate<GameObject>( ControllerPrefab );
+		o.GetComponent<ControllerPanel>().Controller = controller;
+		o.transform.SetParent( transform, false );
+		Controllers.Add( o.GetComponent<ControllerPanel>() );
+	}
+}
---
+++ Assets/ControllerPanel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPanel : MonoBehaviour
+{
+	public Steamworks.Controller Controller;
+
+	public UnityEngine.UI.Text DebugText;
+
+
+	private void Update()
+	{
+		Controller.ActionSet = "InGameControls";
+
+		DebugText.text = $"{Controller.InputType}";
+
+		DigitalState( "fire" );
+		DigitalState( "Jump" );
+		DigitalState( "pause_menu" );
+		AnalogState( "Move" );
+	}
+
+	private void AnalogState( string v )
+	{
+		var state = Controller.GetAnalogState( v );
+
+		DebugText.text += $"\n{state.X} {state.Y}";
+	}
+
+	private void DigitalState( string v )
+	{
+		var state = Controller.GetDigitalState( v );
+		if ( state.Pressed )
+		{
+			DebugText.text += $"\n{v}";
+		}
+	}
+}
